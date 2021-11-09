1850f14a0d4afa713912688def212d6f595ec795
smirnovGleb16 <gleb.s.smirnov@gmail.com>
2021-11-09 14:53:54 +0400

    lesson7_note the begining

--- app/src/main/java/com/example/geekbrains_androidforbegginers/MainActivity.java
+++ app/src/main/java/com/example/geekbrains_androidforbegginers/MainActivity.java
@@ -10,5 +10,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.main_container, new MainFragment())
+                .commit();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getSupportFragmentManager().getFragments().size() > 0) {
+            getSupportFragmentManager()
+                    .popBackStack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
